b72721ff34fa05430e4bb34c0bef16d35b582f4d
dustin <dev.deokhyun@gmail.com>
2021-10-25 18:50:34 +0900

    김덕현: user모듈 완성

---
+++ src/pages/Login.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Login = props => {
+  return <div>로그인</div>;
+};
+
+export default Login;
--- src/redux/modules/user.js
+++ src/redux/modules/user.js
@@ -1,43 +1,61 @@
-import { produce } from 'immer';
+import { createAction, handleActions } from "redux-actions";
+import { produce } from "immer";
+import { apis } from "../../shared/axios";
 
-export const LOGIN = 'user/LOGIN';
-export const LOG_OUT = 'user/LOG_OUT';
-export const AUTHORIZED = 'user/AUTHORIZED';
+const SIGN_UP = "SIGN_UP";
+const LOG_IN = "LOG_IN";
+const LOG_OUT = "LOG_OUT";
 
-export const login = (payload) => ({
-  type: LOGIN,
-  payload,
-});
-
-const logOut = () => ({ type: LOG_OUT });
-
-const authorize = (email, nickname) => ({
-  type: AUTHORIZED,
-  payload: { email, nickname },
-});
+const signUp = createAction(SIGN_UP);
+const logIn = createAction(LOG_IN, user => ({ user }));
+const logOut = createAction(LOG_OUT);
 
 const initialState = {
-  email: '',
-  username: '',
-  isAuthorized: false,
+  user: null,
+  isLoggedIn: false,
+  emailValidation: false,
+  Error: "",
 };
 
-export default function userReducer(state = initialState, action) {
-  return produce(state, (draft) => {
-    switch (action.type) {
-      case LOGIN:
-      case AUTHORIZED: {
-        console.log('AUTHORIZED');
-        console.log(action.payload);
-        break;
-      }
-      case LOG_OUT: {
-        console.log('AUTHORIZED');
-        console.log(action.payload);
-        break;
-      }
-      default:
-        break;
-    }
-  });
-}
+export const signUpAPI = user => {
+  return function (dispatch, getState, { history }) {
+    console.log(user);
+  };
+};
+
+export const logInAPI = user => {
+  return function (dispatch, getState, { history }) {
+    console.log(user);
+  };
+};
+
+export const logOutAPI = () => {
+  return function (dispatch, getState, { history }) {
+    localStorage.removeItem("token");
+    dispatch(logOut());
+    history.replace("/");
+  };
+};
+
+export const getUserAPI = () => {
+  return function (dispatch, getState, { history }) {
+    console.log("getUserAPI");
+  };
+};
+
+export default handleActions({
+  [SIGN_UP]: (state, action) =>
+    produce(state, draft => {
+      console.log(action);
+    }),
+  [LOG_IN]: (state, action) =>
+    produce(state, draft => {
+      draft.user = action.payload.user;
+      draft.isLoggedIn = true;
+    }),
+  [LOG_OUT]: (state, action) =>
+    produce(state, draft => {
+      draft.user = null;
+      draft.isLoggedIn = false;
+    }),
+});
