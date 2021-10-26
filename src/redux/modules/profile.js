15c9f17f712d25ffc32dc8ad1688f5579f51b2c9
dustin <dev.deokhyun@gmail.com>
2021-10-27 00:22:04 +0900

    김덕현: 프로필 업데이트 모듈 작업

--- src/redux/modules/profile.js
+++ src/redux/modules/profile.js
@@ -2,61 +2,42 @@ import { createAction, handleActions } from "redux-actions";
 import { produce } from "immer";
 import { apis } from "../../shared/axios";
 
-// actions Type
-const UPLOADING = "UPLOADING";
-const UPLOAD_IMAGE = "UPLOAD_IMAGE";
-const SET_PREVIEW = "SET_PREVIEW";
+const UPDATE_PROFILE = "UPDATE_PROFILE";
 
-// action creators
-const uploading = createAction(UPLOADING, (uploading) => ({ uploading }));
-const uploadImage = createAction(UPLOAD_IMAGE, (image_url) => ({ image_url }));
-const setPreview = createAction(SET_PREVIEW, (preview) => ({ preview }));
+const updateProfile = createAction(UPDATE_PROFILE, profile => ({ profile }));
 
-//initialState
 const initialState = {
-  image_url: "",
-  uploading: false,
-  preview: null,
+  profile: null,
 };
 
-//middleware
-// const updateProfileAPI = () => {
-//   return function (dispatch, getState, { history }) {
-//     let image = imgInput.current?.files[0];
-//     const upload = storage.ref(`images/${image.name}`).put(image);
+export const updateProfileAPI = profile => {
+  return function (dispatch, getState, { history }) {
+    console.log(profile);
 
-//     upload.then((snapshot) => {
-//       console.log(snapshot);
-//     });
-//   };
-// };
+    apis
+      .updateProfile(profile)
+      .then(res => {
+        console.log(res);
+        dispatch(updateProfile(res.data.data.user));
+      })
+      .catch(res => {
+        console.log(res.response);
+      });
+  };
+};
 
-//reducer
 export default handleActions(
   {
-    [UPLOAD_IMAGE]: (state, action) =>
-      produce(state, (draft) => {
-        draft.image_url = action.payload.image_url;
-        draft.uploading = false;
-      }),
-
-    [UPLOADING]: (state, action) =>
-      produce(state, (draft) => {
-        draft.uploading = action.payload.uploading;
-      }),
-
-    [SET_PREVIEW]: (state, action) =>
-      produce(state, (draft) => {
-        draft.preview = action.payload.preview;
+    [UPDATE_PROFILE]: (state, action) =>
+      produce(state, draft => {
+        draft.profile = action.payload.profile;
       }),
   },
   initialState
 );
 
-//action creator export
 const profileActions = {
-  uploadImage,
-  setPreview,
+  updateProfileAPI,
 };
 
 export { profileActions };
