45dce99fdca7df2b0195eeb68d6f2ddec8624515
bomboming <aosldk09@naver.com>
2021-11-03 20:03:26 +0900

    박새봄 : 평점 구현 완료

--- src/pages/Review.js
+++ src/pages/Review.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import { useSelector, useDispatch } from "react-redux";
 import { actionCreators as reviewAction } from "../redux/modules/review";
+import { FaStar } from "react-icons/fa";
 
 const Review = (props) => {
   const dispatch = useDispatch();
@@ -20,6 +21,27 @@ const Review = (props) => {
     dispatch(reviewAction.addReviewAPI(review));
   };
 
+  //평점
+  const colors = {
+    orange: "#FFBA5A",
+    gray: "#a9a9a9",
+  };
+  const [currentValue, setCurrentValue] = useState(0);
+  const [hoverValue, setHoverValue] = useState(undefined);
+  const stars = Array(5).fill(0);
+
+  const handleClick = (value) => {
+    setCurrentValue(value);
+  };
+
+  const handleMouseOver = (newHoverValue) => {
+    setHoverValue(newHoverValue);
+  };
+
+  const handleMouseLeave = () => {
+    setHoverValue(undefined);
+  };
+
   return (
     <Wrapper>
       <ReviewBox>
@@ -32,7 +54,31 @@ const Review = (props) => {
                 <img src="img/profile.png" style={{ borderRadius: "50%" }} />
                 <span>닉네임</span>
               </User>
-              <RatingBox></RatingBox>
+              <RatingBox>
+                <h2> 닉네임님과의 식사는 어떠셨나요? </h2>
+                <div style={styles.stars}>
+                  {stars.map((_, index) => {
+                    return (
+                      <FaStar
+                        key={index}
+                        size={24}
+                        onClick={() => handleClick(index + 1)}
+                        onMouseOver={() => handleMouseOver(index + 1)}
+                        onMouseLeave={handleMouseLeave}
+                        color={
+                          (hoverValue || currentValue) > index
+                            ? colors.orange
+                            : colors.gray
+                        }
+                        style={{
+                          marginRight: 10,
+                          cursor: "pointer",
+                        }}
+                      />
+                    );
+                  })}
+                </div>
+              </RatingBox>
             </UserInfo>
             <Comment>
               <input onChange={onChangeContent} placeholder="후기작성란" />
@@ -98,4 +144,10 @@ const Comment = styled.div`
   display: flex;
 `;
 
+const styles = {
+  stars: {
+    display: "flex",
+    flexDirection: "row",
+  },
+};
 export default Review;
