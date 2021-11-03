406b4da1b2c9f1935f8f433464a50c52ad75a26b
bomboming <aosldk09@naver.com>
2021-11-03 12:45:13 +0900

    임시저장

---
+++ src/pages/Review.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Review = (props) => {
+  return <React.Fragment>리뷰페이지입니다</React.Fragment>;
+};
+
+export default Review;
--- src/shared/App.js
+++ src/shared/App.js
@@ -14,6 +14,7 @@ import Profile from "../pages/Profile";
 import LunchCreateUpdate from "../pages/LunchCreateUpdate";
 import ProfileUpdate from "../pages/ProfileUpdate";
 import LunchDetail from "../pages/LunchDetail";
+import Review from "../pages/Review";
 
 import Header from "../components/Header";
 import Footer from "../components/Footer";
@@ -62,6 +63,7 @@ function App() {
           ></Route>
           <Route path="/memberlist" exact component={MemberList}></Route>
           <Route path="/notification" exact component={Notification}></Route>
+          <Route path="/review" exact component={Review}></Route>
         </Switch>
         {/* <Footer /> */}
       </ConnectedRouter>
