75a786e849a9877b7f7ee3ac5e59eb526b34dc5a
bomboming <aosldk09@naver.com>
2021-11-10 17:32:43 +0900

    메인 카드 수정

--- src/components/LunchNew.js
+++ src/components/LunchNew.js
@@ -1,26 +1,45 @@
 import React from "react";
 import styled from "styled-components";
 import { Grid } from "../elements";
+import { history } from "../redux/configureStore";
+import moment from "moment";
+import "moment/locale/ko";
 
-const LunchNew = props => {
+import BookmarkImg from "../assets/bookmark.svg";
+
+const LunchNew = (props) => {
+  const { title, host, lunchid, date, locations, membernum, applicants } =
+    props;
+  const strDate = String(date);
+  const schedule = moment(strDate).format("YYYY-MM-DD(ddd)");
+  const scheduleTime = moment(strDate).format("A hh시 mm분");
+  const adressDong = locations.address_name.split(" ")[2];
+
+  console.log("이거예욤", applicants);
+  console.log("이거예욤22", props);
+
   return (
     <>
-      <Wrapper padding="2rem" margin="0 0 rem 0" shadow>
+      <Wrapper
+        onClick={() => {
+          history.push(`/lunchpost/${lunchid}`);
+        }}
+      >
         <ELWrapper
           margin="0 0 1rem 0"
           flex
           style={{ justifyContent: "space-between" }}
         >
-          <Text weight="600" color="black" size="1.4" color="#FFC428">
-            삼성동&nbsp;&nbsp;|&nbsp;&nbsp; 12시
+          <Text weight="600" size="1.4" color="#FFC428">
+            {adressDong}&nbsp;&nbsp;|&nbsp;&nbsp; {scheduleTime}
           </Text>
           <Text weight="800" size="1.4">
-            3&nbsp;/&nbsp;4
+            2&nbsp;&nbsp;/&nbsp;&nbsp;{membernum}
           </Text>
         </ELWrapper>
-        <ELWrapper margin="0 0 2rem 0">
+        <ELWrapper margin="0 0 2rem 0" style={{ overflowY: "hidden" }}>
           <Text weight="700" size="2" color="black">
-            코엑스 에그슬럿에서 가볍고, 빠르게!
+            {title}
           </Text>
         </ELWrapper>
         <ELWrapper flex margin="0 0 2rem 0">
@@ -30,9 +49,9 @@ const LunchNew = props => {
           />
           <ELWrapper>
             <Text weight="600" color="black" size="1.4">
-              gizmo7duck
+              {host.nickname}
             </Text>
-            <Text size="1.4">프론트엔드 개발자</Text>
+            <Text size="1.4">{host.job}</Text>
           </ELWrapper>
         </ELWrapper>
         <ELWrapper margin="0 0 2rem 0">
@@ -40,11 +59,11 @@ const LunchNew = props => {
         </ELWrapper>
         <ELWrapper flex style={{ justifyContent: "space-between" }}>
           <ELWrapper>
-            <Text size="1.4">📍&nbsp;&nbsp; 코엑스 에그슬럿</Text>
-            <Text size="1.4">📆&nbsp;&nbsp; 2021년 11월 10일(수)</Text>
+            <Text size="1.4">📍&nbsp;&nbsp; {locations.place_name}</Text>
+            <Text size="1.4">📆&nbsp;&nbsp; {schedule}</Text>
           </ELWrapper>
           <Bookmark>
-            <img src="/img/bookmark.svg" />
+            <img src={BookmarkImg} />
             <span>3</span>
           </Bookmark>
         </ELWrapper>
@@ -54,36 +73,31 @@ const LunchNew = props => {
 };
 
 const Wrapper = styled.div`
-  width: ${props => props.width};
-  height: 100%;
-  ${props => (props.padding ? `padding: ${props.padding};` : "")};
-  ${props => (props.margin ? `margin: ${props.margin};` : "")};
-  background-color: ${props => (props.bg ? props.bg : "white")};
-  ${props => (props.flex ? `display: flex; align-items: center; ` : "")};
-  ${props => (props.center ? `text-align: center` : "")};
-  ${props =>
-    props.shadow ? `box-shadow: 5px 5px 5px 2px rgba(55, 50, 40, 0.16)` : ""};
+  width: 280px;
+  height: 270px;
+  padding: 2rem;
+  background-color: white;
+  box-shadow: 5px 5px 5px 2px rgba(55, 50, 40, 0.16);
+  display: flex;
+  flex-direction: column;
   border-radius: 10px;
-  max-width: 280px;
-  max-height: 270px;
 `;
 
 const ELWrapper = styled.div`
-  height: 100%;
-  ${props => (props.padding ? `padding: ${props.padding};` : "")};
-  ${props => (props.margin ? `margin: ${props.margin};` : "")};
-  background-color: ${props => (props.bg ? props.bg : "white")};
-  ${props => (props.flex ? `display: flex; align-items: center; ` : "")};
-  ${props => (props.center ? `text-align: center` : "")};
-  ${props =>
+  ${(props) => (props.padding ? `padding: ${props.padding};` : "")};
+  ${(props) => (props.margin ? `margin: ${props.margin};` : "")};
+  background-color: ${(props) => (props.bg ? props.bg : "white")};
+  ${(props) => (props.flex ? `display: flex; align-items: center; ` : "")};
+  ${(props) => (props.center ? `text-align: center` : "")};
+  ${(props) =>
     props.shadow ? `box-shadow: 5px 5px 5px 2px rgba(55, 50, 40, 0.16)` : ""};
   align-items: center;
 `;
 
 const Text = styled.p`
-  font-size: ${props => (props.size ? props.size : "1.6")}rem;
-  font-weight: ${props => (props.weight ? props.weight : "400")};
-  color: ${props => (props.color ? props.color : "#909090")};
+  font-size: ${(props) => (props.size ? props.size : "1.6")}rem;
+  font-weight: ${(props) => (props.weight ? props.weight : "400")};
+  color: ${(props) => (props.color ? props.color : "#909090")};
   overflow: hidden;
   text-overflow: ellipsis;
   /* white-space: nowrap; */
@@ -92,11 +106,11 @@ const Text = styled.p`
 `;
 
 const CircleImage = styled.div`
-  width: ${props => props.size}rem;
-  height: ${props => props.size}rem;
-  border-radius: ${props => props.size}rem;
+  width: ${(props) => props.size}rem;
+  height: ${(props) => props.size}rem;
+  border-radius: ${(props) => props.size}rem;
 
-  background-image: url("${props =>
+  background-image: url("${(props) =>
     props.src
       ? props.src
       : "http://webimage.10x10.co.kr/image/basic600/165/B001654412.jpg"}");
