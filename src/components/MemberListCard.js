8da6dbf0b27db2f1e9db10ed57d09ada9df7ae98
minguk <minkuk90@gmail.com>
2021-11-11 09:46:55 +0900

    [PR] : 아침 PR

--- src/components/MemberListCard.js
+++ src/components/MemberListCard.js
@@ -3,21 +3,31 @@ import styled from "styled-components";
 
 import { Avatar } from "@mui/material";
 
+import { history } from "../redux/configureStore";
+
 const MemberListCard = () => {
   return (
     <>
-      <MemberCard>
+      <MemberCard
+        onClick={() => {
+          history.push(`profile/1975496891`);
+        }}
+      >
         <CardTop>
           <Avatar sx={{ width: "60px", height: "60px" }} />
           <MemberInfo>
             <MemberName>화정</MemberName>
-            <p>디자이너</p>
+            <Memberjob>디자이너</Memberjob>
           </MemberInfo>
         </CardTop>
-        <MemberDesc>
-          typesetting industry. Lorem Ipsum has been the industry's standard
-          dummy text ever since the 1500s, when an unknown printer took a galley
-        </MemberDesc>
+        <CardBottom>
+          <MemberDesc>
+            typesetting industry. Lorem Ipsum has been the industry's standard
+            dummy text ever since the 1500s, when an unknown printer took a
+            galley
+          </MemberDesc>
+          <MemberButton>신청하기</MemberButton>
+        </CardBottom>
       </MemberCard>
     </>
   );
@@ -25,17 +35,23 @@ const MemberListCard = () => {
 
 const MemberCard = styled.div`
   box-sizing: border-box;
-  padding: 10px;
+  padding: 20px 10px 10px 10px;
   background-color: white;
   border-radius: 10px;
-  max-width: 290px;
+  width: 290px;
   margin: 5px;
+  cursor: pointer;
+  box-shadow: 0px 5px 7px -7px rgba(0, 0, 0, 0.75);
+  &:hover {
+    background-color: #f2f4f6;
+  }
   @media only screen and (max-width: 768px) {
-    max-width: 100%;
+    width: 100%;
   }
 `;
 
 const CardTop = styled.div`
+  margin: 15px 0;
   display: flex;
   align-items: center;
 `;
@@ -47,19 +63,42 @@ const MemberInfo = styled.div`
 const MemberName = styled.p`
   font-size: 18px;
   font-weight: bold;
+  margin-bottom: 8px;
+`;
+
+const Memberjob = styled.p`
+  font-size: 16px;
+  color: #9d9d9d;
+`;
+
+const CardBottom = styled.div`
+  display: flex;
+  flex-direction: column;
 `;
 
 const MemberDesc = styled.p`
-  height: 100px;
+  height: 60px;
   display: -webkit-box;
   -webkit-box-orient: vertical;
-  -webkit-line-clamp: 3;
+  -webkit-line-clamp: 2;
   overflow: hidden;
   margin-top: 10px;
   font-size: 16px;
   padding: 5px;
-  line-height: 3rem;
+  line-height: 30px;
   overflow: hidden;
+`;
+
+const MemberButton = styled.button`
+  padding: 7px 15px;
+  margin: 25px 5px 10px 5px;
+  height: 37px;
+  color: #ed6653;
+  border: none;
+  border-radius: 5px;
+  font-size: 14px;
+  background-color: #ffedee;
+  box-shadow: 0px 5px 7px -7px rgba(0, 0, 0, 0.75);
 `;
 
 export default MemberListCard;
