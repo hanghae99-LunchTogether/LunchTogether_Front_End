2cdc2604428ba917ef4b2bfec6e43c65cb793488
minguk <minkuk90@gmail.com>
2021-11-12 19:16:45 +0900

    [feat] : 푸터 로고클릭시 최상단

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -52,7 +52,11 @@ const Footer = (props) => {
                 <div>깃주소</div>
               </Git>
             </Member>
-            <C>
+            <C
+              onClick={() => {
+                window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+              }}
+            >
               <img src={LogoImg} />
               <span>© 2021 Project Fork. All rights reserved.</span>
             </C>
@@ -193,7 +197,7 @@ const C = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-
+  cursor: pointer;
   img {
     width: 8rem;
     height: 8rem;
