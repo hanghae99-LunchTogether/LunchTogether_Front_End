dc8d0238657e08993b3c8d04304b7408b99ab50f
deokhyun-dv <dev.deokhyun@gmail.com>
2021-11-30 20:32:17 +0900

    feat: 로그아웃 수정

--- src/components/Header.js
+++ src/components/Header.js
@@ -17,7 +17,10 @@ let socket;
 
 const Header = (props) => {
   const dispatch = useDispatch();
+  console.log(history);
   const user = useSelector((state) => state.user.user);
+  console.log(user);
+  const token = localStorage.getItem("token");
 
   const { Kakao } = window;
 
@@ -26,6 +29,8 @@ const Header = (props) => {
       console.log(Kakao.Auth.getAccessToken());
     });
     dispatch(userActions.logOutAPI());
+    localStorage.removeItem("token");
+    history.go();
   };
 
   //socket
