7b263758e812d0af71179ffba4759f1a024e0a04
deokhyun-dv <dev.deokhyun@gmail.com>
2021-12-03 16:25:56 +0900

    fix: 런치카드 인원 컬러 바꿈

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -24,7 +24,7 @@ const Home = (props) => {
       return;
     }
     const data = await apis.getLunchListMain(page);
-    const lunchs = data.data;
+    const lunchs = data?.data;
     setLunchList([...lunchList, ...lunchs]);
     if (lunchs.length < 11) {
       setNext(false);
